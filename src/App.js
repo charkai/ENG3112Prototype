8a07d666f0758cfc28d63659a353abe5646a6eaa
soft3888_m10_03_p11-admin <ckai8237@uni.sydney.edu.au>
2025-03-31 13:29:47 +1100

    adding emojis to homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import Work from './Work';
+import Personal from './Personal';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] dark:bg-gray-900 dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(255,255,255,0.3),rgba(0,0,0,0))]">
+		<header className="App-header">
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/work" element={<Work />}/>
+					<Route path="/personal" element={<Personal />}/>
+				</Routes>
+			</Router>
+		</header>
     </div>
   );
 }
